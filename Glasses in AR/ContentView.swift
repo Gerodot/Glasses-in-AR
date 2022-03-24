623d3ddeeb324b9ca529fe0b319262dee0f0496f
Gerodot <gerodot@me.com>
2022-03-24 15:47:45 +0300

    Added face tracking configuration

--- Glasses in AR/ContentView.swift
+++ Glasses in AR/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Gerodot on 22.03.2022.
 //
 
+import ARKit
 import SwiftUI
 import RealityKit
 
@@ -17,8 +18,21 @@ struct ContentView : View {
 struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
-        
+        // Create ARView
         let arView = ARView(frame: .zero)
+        
+        // Check face tracking conf is supported
+        guard ARFaceTrackingConfiguration.isSupported else {
+            print(#line,#function, "Sorry your device is ont supported face tracking")
+            return arView
+        }
+        
+        //Create face tracking config
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        
+        //Run face tracking conf
+        arView.session.run(configuration, options: [])
         
         return arView
         
